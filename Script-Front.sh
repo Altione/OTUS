4e556877a3c44b202f9c5c72f22ab8fbd05477a3
Altione <altione@bk.ru>
2025-02-05 10:52:08 +0300

    Сделал test

--- Script-Front.sh
+++ Script-Front.sh
@@ -2,7 +2,7 @@
 apt install nginx -y;
 
 ### Скачать nginx site-enable default и переместить
-wget https://raw.githubusercontent.com/Altione/OTUS-Front/refs/heads/main/nginx-sites-available;
+wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/nginx-sites-available;
 cp nginx-sites-available /etc/nginx/sites-available/default;
 
 ### рестарт nginx
