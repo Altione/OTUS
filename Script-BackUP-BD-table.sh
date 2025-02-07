343c114b4e1a5cdc5efb16831f1f2dbb9c07f63b
Altione <altione@bk.ru>
2025-02-07 16:53:25 +0300

    bd table script

--- Script-BackUP-BD-table.sh
+++ Script-BackUP-BD-table.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 USER="root"
-PASSWORD="Testpass1$"
 OUTPUT_DIR="/home/altione/DB/DB"
 
 # Получаем БД
-databases=$(mysql -u"$USER" -p"$PASSWORD" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
+databases=$(mysql -u"$USER" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 # Цикл по каждой БД
 for db in $databases; do
@@ -15,14 +14,14 @@ for db in $databases; do
     mkdir -p "$OUTPUT_DIR/$db"
 
     # Получаем список всех таблиц в БД
-    tables=$(mysql -u"$USER" -p"$PASSWORD" -e "SHOW TABLES IN $db;" | tr -d "| " | grep -v Tables_in)
+    tables=$(mysql -u"$USER" -e "SHOW TABLES IN $db;" | tr -d "| " | grep -v Tables_in)
 
     # Цикл по каждой таблице
     for table in $tables; do
         echo "Выгружаем таблицу: $table из базы $db"
 
         # Делаем выгрузку таблицы в файл внутри папки базы данных
-        mysqldump -u"$USER" -p"$PASSWORD" "$db" "$table" > "$OUTPUT_DIR/$db/$table"
+        mysqldump -u"$USER" "$db" "$table" > "$OUTPUT_DIR/$db/$table"
     done
 done
 
