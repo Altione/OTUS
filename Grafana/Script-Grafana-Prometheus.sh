986798f03e7beeb22dfd0be0ca2f12666fb16c89
Altione <altione@bk.ru>
2025-02-14 13:02:31 +0300

    install Grafana

--- Grafana/Script-Grafana-Prometheus.sh
+++ Grafana/Script-Grafana-Prometheus.sh
@@ -7,5 +7,7 @@ wget https://dl.grafana.com/oss/release/grafana_11.5.1_amd64.deb
 sudo dpkg -i grafana_11.5.1_amd64.deb
 
 #рестартуем демона, стартуем сервер (admin/admin)
+sleep 3;
 systemctl daemon-reload;
+sleep 3;
 systemctl start grafana-server;
