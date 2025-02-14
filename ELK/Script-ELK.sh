9abc6379f60210aa4181f07b4b10225c47762e22
Altione <altione@bk.ru>
2025-02-14 12:44:26 +0300

    install ELK

--- ELK/Script-ELK.sh
+++ ELK/Script-ELK.sh
@@ -1,3 +1,4 @@
+### закинул deb пакеты в home
 ### для работы нужен jdk
 apt install default-jdk -y;
 
@@ -11,3 +12,28 @@ cat > /etc/elasticsearch/jvm.options.d/jvm.options
 -Xms1g
 -Xmx1g
 
+
+### скачиваем конфиги
+wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/elasticsearch.yml
+wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/filebeat.yml
+wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/kibana.yml
+wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/logstash.yml
+wget https://raw.githubusercontent.com/Altione/OTUS/refs/heads/main/ELK/logstash-nginx-es.conf
+
+
+### копируем в нужные папки
+cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+cp filebeat.yml /etc/filebeat/filebeat.yml
+cp kibana.yml /etc/kibana/kibana.yml
+cp logstash.yml /etc/logstash/logstash.yml
+cp logstash-nginx-es.conf /etc/logstash/conf.d/logstash-nginx-es.conf
+
+### рестартуем все возможное
+systemctl daemon-reload
+systemctl enable --now elasticsearch.service
+systemctl daemon-reload
+systemctl enable --now kibana.service
+systemctl enable --now logstash.service
+systemctl restart logstash.service
+systemctl restart filebeat
+systemctl enable filebeat
