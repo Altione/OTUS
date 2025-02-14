3d012c22db549945fc02c969e635eb49252d37a3
Altione <altione@bk.ru>
2025-02-14 11:35:39 +0300

    install Grafana

---
+++ Grafana/Script-Grafana-Prometheus.sh
@@ -0,0 +1,11 @@
+### установка прометея порты 9100 9090
+apt install prometheus -y;
+
+#установка графаны OSS
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/oss/release/grafana_11.5.1_amd64.deb
+sudo dpkg -i grafana_11.5.1_amd64.deb
+
+#рестартуем демона, стартуем сервер (admin/admin)
+systemctl daemon-reload;
+systemctl start grafana-server;
