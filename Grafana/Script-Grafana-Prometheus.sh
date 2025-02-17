eb5c8dcd9ee94264f2a29e6749bc9eb2609a6628
Altione <55092915+Altione@users.noreply.github.com>
2025-02-17 11:54:04 +0300

    Update Script-Grafana-Prometheus.sh

--- Grafana/Script-Grafana-Prometheus.sh
+++ Grafana/Script-Grafana-Prometheus.sh
@@ -3,11 +3,10 @@ apt install prometheus -y;
 
 #установка графаны OSS
 sudo apt-get install -y adduser libfontconfig1 musl
-wget https://dl.grafana.com/oss/release/grafana_11.5.1_amd64.deb
 sudo dpkg -i grafana_11.5.1_amd64.deb
 
 #рестартуем демона, стартуем сервер (admin/admin)
 sleep 3;
 systemctl daemon-reload;
-sleep 10;
+sleep 3;
 systemctl start grafana-server;
